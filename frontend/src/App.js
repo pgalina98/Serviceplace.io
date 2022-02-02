7d444aea7ae484b23e0cdc436b8ed6c20568858f
Patrik Galina <patrik.galina@king-ict.hr>
2022-02-02 21:32:03 +0100

    feat: Create initial styling

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,14 @@
+import React from "react";
+import Homepage from "./pages/homepage";
+import Sidebar from "./components/sidebar/sidebar";
+
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Homepage />
+      <Sidebar />
+    </div>
+  );
 }
 
 export default App;
