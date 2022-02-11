8b7950407cf10cbdda61f1d69007349dce6400b7
Patrik Galina <patrik.galina@king-ict.hr>
2022-02-11 18:11:46 +0100

    chore: Create custom dispatcher with logger

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -2,6 +2,21 @@ import { createStore, combineReducers } from "redux";
 
 import serviceReducer from "reducers/service-reducer";
 
+const initializeDispatchWithLogger = (store) => {
+  const dispatch = store.dispatch;
+
+  return (action) => {
+    console.group(action.type);
+    console.log("[DISPATCHED ACTION]:", action);
+    console.log("[PREVIOUS STATE]:", store.getState());
+    const dispatchedAction = dispatch(action);
+    console.log("[NEW STATE]:", store.getState());
+    console.groupEnd(action.type);
+
+    return dispatchedAction;
+  };
+};
+
 const initializeStore = () => {
   const combinedReducers = combineReducers({
     serviceState: serviceReducer,
@@ -11,6 +26,8 @@ const initializeStore = () => {
     combinedReducers,
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   );
+
+  store.dispatch = initializeDispatchWithLogger(store);
 
   return store;
 };
