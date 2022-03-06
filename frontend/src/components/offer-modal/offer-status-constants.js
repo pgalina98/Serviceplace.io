a96ac7be5c018a01e5612fee77479bedfeb6b8d0
Patrik Galina <patrik.galina@king-ict.hr>
2022-03-06 21:18:56 +0100

    chore: Create mapper for offer status

--- frontend/src/components/offer-modal/offer-status-constants.js
+++ frontend/src/components/offer-modal/offer-status-constants.js
@@ -1,5 +1,23 @@
+export const OFFER_STATUS_PENDING = "PENDING";
+export const OFFER_STATUS_REJECTED = "REJECTED";
+export const OFFER_STATUS_ACCEPTED = "ACCEPTED";
+
 export const OFFER_STATUS = {
   PENDING: 1,
   REJECTED: 2,
   ACCEPTED: 3,
+};
+
+export const mapIdToStatus = (statusId) => {
+  switch (statusId) {
+    case OFFER_STATUS.PENDING:
+      return OFFER_STATUS_PENDING;
+    case OFFER_STATUS.REJECTED:
+      return OFFER_STATUS_REJECTED;
+    case OFFER_STATUS.ACCEPTED:
+      return OFFER_STATUS_ACCEPTED;
+
+    default:
+      return null;
+  }
 };
