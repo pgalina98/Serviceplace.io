7215aa61bb243086569ebf50a2137cc05d42b0e6
Patrik Galina <patrik.galina@king-ict.hr>
2022-03-08 20:02:05 +0100

    fix: Fix problem with resolving array of promises

--- frontend/src/actions/offer-actions.js
+++ frontend/src/actions/offer-actions.js
@@ -7,63 +7,65 @@ export const saveOffer = async (offer) => {
 };
 
 export const getCreatedOffers = (userId) => {
-  return api.fetchCreatedOffers(userId).then((response) => {
-    return response.docs.map(async (document) => {
-      const fromUserRef = document.data()["fromUserRef"];
-      const toUserRef = document.data()["toUserRef"];
-      const serviceRef = document.data()["serviceRef"];
+  return api.fetchCreatedOffers(userId).then(async (response) => {
+    return await Promise.all(
+      response.docs.map(async (document) => {
+        const fromUserRef = document.data()["fromUserRef"];
+        const toUserRef = document.data()["toUserRef"];
+        const serviceRef = document.data()["serviceRef"];
 
-      const fromUser = await getDoc(fromUserRef);
-      const toUser = await getDoc(toUserRef);
-      const service = await getDoc(serviceRef);
+        const fromUser = await getDoc(fromUserRef);
+        const toUser = await getDoc(toUserRef);
+        const service = await getDoc(serviceRef);
 
-      const offer = { id: document.id, ...document.data() };
+        const offer = { id: document.id, ...document.data() };
 
-      delete Object.assign(offer, {
-        fromUser: { id: fromUser.id, ...fromUser.data() },
-      })["fromUserRef"];
+        delete Object.assign(offer, {
+          fromUser: { id: fromUser.id, ...fromUser.data() },
+        })["fromUserRef"];
 
-      delete Object.assign(offer, {
-        toUser: { id: toUser.id, ...toUser.data() },
-      })["toUserRef"];
+        delete Object.assign(offer, {
+          toUser: { id: toUser.id, ...toUser.data() },
+        })["toUserRef"];
 
-      delete Object.assign(offer, {
-        service: { id: service.id, ...service.data() },
-      })["serviceRef"];
+        delete Object.assign(offer, {
+          service: { id: service.id, ...service.data() },
+        })["serviceRef"];
 
-      return offer;
-    });
+        return offer;
+      })
+    );
   });
 };
 
 export const getReceivedOffers = (userId) => {
-  return api.fetchReceivedOffers(userId).then((response) => {
-    const offers = response.docs.map(async (document) => {
-      const fromUserRef = document.data()["fromUserRef"];
-      const toUserRef = document.data()["toUserRef"];
-      const serviceRef = document.data()["serviceRef"];
+  return api.fetchReceivedOffers(userId).then(async (response) => {
+    return await Promise.all(
+      response.docs.map(async (document) => {
+        const fromUserRef = document.data()["fromUserRef"];
+        const toUserRef = document.data()["toUserRef"];
+        const serviceRef = document.data()["serviceRef"];
 
-      const fromUser = await getDoc(fromUserRef);
-      const toUser = await getDoc(toUserRef);
-      const service = await getDoc(serviceRef);
+        const fromUser = await getDoc(fromUserRef);
+        const toUser = await getDoc(toUserRef);
+        const service = await getDoc(serviceRef);
 
-      const offer = { id: document.id, ...document.data() };
+        const offer = { id: document.id, ...document.data() };
 
-      delete Object.assign(offer, {
-        fromUser: { id: fromUser.id, ...fromUser.data() },
-      })["fromUserRef"];
+        delete Object.assign(offer, {
+          fromUser: { id: fromUser.id, ...fromUser.data() },
+        })["fromUserRef"];
 
-      delete Object.assign(offer, {
-        toUser: { id: toUser.id, ...toUser.data() },
-      })["toUserRef"];
+        delete Object.assign(offer, {
+          toUser: { id: toUser.id, ...toUser.data() },
+        })["toUserRef"];
 
-      delete Object.assign(offer, {
-        service: { id: service.id, ...service.data() },
-      })["serviceRef"];
+        delete Object.assign(offer, {
+          service: { id: service.id, ...service.data() },
+        })["serviceRef"];
 
-      return offer;
-    });
-
-    return offers;
+        return offer;
+      })
+    );
   });
 };
