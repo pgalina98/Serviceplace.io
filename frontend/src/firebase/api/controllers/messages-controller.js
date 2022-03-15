5dce4c460790402e0d9e9f6a3786cd7eac2ec929
Patrik Galina <patrik.galina@king-ict.hr>
2022-03-15 08:03:05 +0100

    chore: Remove fetching data from document references

--- frontend/src/firebase/api/controllers/messages-controller.js
+++ frontend/src/firebase/api/controllers/messages-controller.js
@@ -22,50 +22,11 @@ export const subscribe = (userId, callback) => {
     where("toUserRef", "==", userRef)
   );
 
-  const unsubscribe = onSnapshot(queryGetMessagesByUserId, async (response) => {
-    const messages = await Promise.all(
-      response.docs.map(async (document) => {
-        const fromUserRef = document.data()["fromUserRef"];
-        const toUserRef = document.data()["toUserRef"];
-        const collaborationRef = document.data()["collaborationRef"];
-
-        const fromUser = await getDoc(fromUserRef);
-        const toUser = await getDoc(toUserRef);
-        const collaboration = await getDoc(collaborationRef);
-
-        const offerRef = collaboration.data()["offerRef"];
-
-        const offer = await getDoc(offerRef);
-
-        const serviceRef = offer.data()["serviceRef"];
-
-        const service = await getDoc(serviceRef);
-
-        const message = { id: document.id, ...document.data() };
-
-        delete Object.assign(message, {
-          fromUser: { id: fromUser.id, ...fromUser.data() },
-        })["fromUserRef"];
-
-        delete Object.assign(message, {
-          toUser: { id: toUser.id, ...toUser.data() },
-        })["toUserRef"];
-
-        delete Object.assign(message, {
-          collaboration: {
-            id: collaboration.id,
-            ...collaboration.data(),
-            offer: {
-              id: offer.id,
-              ...offer.data(),
-              service: { id: service.id, ...service.data() },
-            },
-          },
-        })["collaborationRef"];
-
-        return message;
-      })
-    );
+  const unsubscribe = onSnapshot(queryGetMessagesByUserId, (response) => {
+    const messages = response.docs.map((document) => ({
+      id: document.id,
+      ...document.data(),
+    }));
 
     callback(messages, unsubscribe);
   });
