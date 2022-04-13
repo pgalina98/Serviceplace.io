def62b125f9f79ab6cd0dd5a9538af8971eb4869
Patrik Galina <patrik.galina@king-ict.hr>
2022-04-13 23:09:45 +0200

    chore: Save new message into Realtime database

--- frontend/src/actions/message-actions.js
+++ frontend/src/actions/message-actions.js
@@ -1,12 +1,47 @@
-import { serverTimestamp } from "firebase/firestore";
+import {
+  ref,
+  push,
+  onDisconnect,
+  set,
+  get,
+  child,
+  onValue,
+} from "firebase/database";
+
+import database from "../firebase/realtime-database/index";
+
+import dayjs from "dayjs";
+
+import { APP_DATE_WITH_DAY_AND_MONTH_TIME_FORMAT } from "config/date-time-formats";
 
 import * as api from "../firebase/api/controllers/messages-controller";
 
 export const sendNewMessage = (collaboratioId, message) => {
   const newMessage = {
     ...message,
-    createdAt: serverTimestamp(),
+    createdAt: dayjs().format(APP_DATE_WITH_DAY_AND_MONTH_TIME_FORMAT),
   };
+
+  const collaborationRef = ref(
+    database,
+    `colaborations/${collaboratioId}/messages`
+  );
+
+  onValue(
+    collaborationRef,
+    (snapshot) => {
+      if (snapshot.val()) {
+        set(collaborationRef, [...snapshot.val(), newMessage]);
+      } else {
+        set(collaborationRef, [newMessage]);
+      }
+    },
+    {
+      onlyOnce: true,
+    }
+  );
+
+  onDisconnect(collaborationRef).remove();
 
   api.sendMessage(collaboratioId, newMessage);
 };
