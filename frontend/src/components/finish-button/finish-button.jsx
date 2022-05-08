7a7fed7f410e7248cfdb657fe667b388e7225e07
Patrik Galina <patrik.galina@king-ict.hr>
2022-05-08 22:11:48 +0200

    chore: Add jQuery support to fix problem with button animation

--- frontend/src/components/finish-button/finish-button.jsx
+++ frontend/src/components/finish-button/finish-button.jsx
@@ -1,9 +1,36 @@
 import React from "react";
 
+import $ from "jquery";
+
 import "./finish-button.scss";
 
 export const FinishButton = () => {
-  return <button id="finish-button" />;
+  $(function () {
+    $("#finish-button").on("click", function () {
+      $("#finish-button").addClass("clicked");
+      setTimeout(() => {
+        validate();
+      }, 250);
+    });
+
+    function validate() {
+      setTimeout(function () {
+        $("#finish-button").removeClass("clicked");
+        $("#finish-button").addClass("validate");
+        setTimeout(() => {
+          callback();
+        }, 250);
+      }, 2250);
+    }
+
+    function callback() {
+      setTimeout(function () {
+        $("#finish-button").removeClass("validate");
+      }, 1250);
+    }
+  });
+
+  return <button id="finish-button">FINISH</button>;
 };
 
 export default FinishButton;
